cf8fccb2dbd7943da0b9d815d081824fdb703428
SrivalliAvala <srivalliavala999@gmail.com>
2024-11-12 14:50:35 +0530

    eksctl

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-0372233cbe1615ef2"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-06e5d0efeb6ae3ba0"
+}
